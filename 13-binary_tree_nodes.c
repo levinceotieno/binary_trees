324c8c7fbbdb3b747b2a62fb8cee1e999b8bd5c2
levinceotieno <levynceotieno@gmail.com>
2024-01-03 17:09:11 +0300

    changes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,16 +8,12 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t nodes = 0;
-
-    if (tree)
-    {
-        if (tree->left || tree->right)
-            return 0;
-
-        nodes = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1;
-    }
-
-    return (nodes);
+size_t nodes = 0;
+if (tree)
+{
+	if (tree->left || tree->right)
+		return 0;
+	nodes = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1;
 }
-
+return (nodes);
+}
